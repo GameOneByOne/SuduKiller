54f4fb84a8ba50f676a57c4223d89b4b35127bd0
GameOneByOne <yanzhang.a2@gmail.com>
2025-05-25 15:46:41 +0800

    :lipstick: 优化提示按钮的显示文本

--- sudo/static/sudo/sudo_button_func.js
+++ sudo/static/sudo/sudo_button_func.js
@@ -1,4 +1,4 @@
-function resetGame() {
+function resetGame(button) {
     document.querySelectorAll('#sudoku td').forEach(cell => {
         if (!cell.classList.contains('fixed')) {
             cell.textContent = '';
@@ -9,12 +9,14 @@ function resetGame() {
     sudoku.dataset.startTime = null;
 }
 
-function showHint() {
+function showHint(button) {
     document.querySelectorAll('#sudoku-hint').forEach(cell => {
         if (!cell.classList.contains('hidden')) {
             cell.classList.add('hidden');
+            button.textContent = "开启提示";
         } else {
             cell.classList.remove('hidden');
+            button.textContent = "关闭提示";
         }
     });
 }
